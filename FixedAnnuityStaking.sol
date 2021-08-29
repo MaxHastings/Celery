5349651961f46f17e06c824e07c790fc333cc52f
MaxHastings <max.jacob.hastings@gmail.com>
2021-08-29 15:20:03 -0500

    Changed from block number to block timestamp

--- FixedAnnuityStaking.sol
+++ FixedAnnuityStaking.sol
@@ -6,12 +6,14 @@ import "prb-math/contracts/PRBMathUD60x18.sol";
 
 struct Account {
     uint256 stakedAmount;
-    uint256 blockHeight;
-    uint256 withdrawAmountPerBlock;
+    uint256 time;
+    uint256 payoutAmountSnapshot;
     int status;
 }
  
 contract FixedAnnuityStaking is ERC20 {
+    
+    uint256 fakeBlockTimestamp;
     
     mapping(address => Account) private _accounts;
 
@@ -25,12 +27,12 @@ contract FixedAnnuityStaking is ERC20 {
         return _accounts[addr].stakedAmount;
     }
     
-    function getStakedBlockHeight(address addr) public view returns (uint256) {
-        return _accounts[addr].blockHeight;
+    function getStakedTime(address addr) public view returns (uint256) {
+        return _accounts[addr].time;
     }
     
-    function getWithdrawAmountPerBlock(address addr) public view returns (uint256) {
-        return _accounts[addr].withdrawAmountPerBlock;
+    function getPayoutAmountSnapshot(address addr) public view returns (uint256) {
+        return _accounts[addr].payoutAmountSnapshot;
     }
     
     function getStakedStatus(address addr) public view returns (int) {
@@ -58,21 +60,22 @@ contract FixedAnnuityStaking is ERC20 {
     }
     
     //////////////// DEBUG FUNCTIONS BEGIN //////////////////////////////
-    function IncreaseBlockHeight() public returns (uint256) {
-        int a = 1+1;
-        return block.number;
+    function SetFakeBlockTimestamp(uint256 timeStamp) public {
+        fakeBlockTimestamp = timeStamp;
     }
     
-    function ViewBlockHeight() public view returns (uint256) {
-        return block.number;
+    function ViewBlockTimeStamp() public view returns (uint256) {
+        return fakeBlockTimestamp;
+        // return block.timeStamp;
     }
      //////////////// DEBUG FUNCTIONS END //////////////////////////////
     
     function calcStake() private {
-      uint256 lastBlockHeight = _accounts[msg.sender].blockHeight;
-      uint256 blocksStakedNorm = block.number - lastBlockHeight;
-      uint256 blocksStaked = PRBMathUD60x18.fromUint(blocksStakedNorm);
-      uint256 blockNumber = block.number;
+      uint256 lastTime = _accounts[msg.sender].time;
+      //uint256 timeStamp = block.timeStamp;
+      uint256 timeStamp = fakeBlockTimestamp;
+      uint256 secondsStakedNorm = timeStamp - lastTime;
+      uint256 secondsStaked = PRBMathUD60x18.fromUint(secondsStakedNorm);
       
       // Annual interest of 363% continuous compounding.
       // approx. 1% daily compounding.
@@ -80,17 +83,18 @@ contract FixedAnnuityStaking is ERC20 {
       uint256 interest = 3630000000000000000;
       // Euler's number represented as 60.18-decimal fixed-point number
       uint256 euler = 2718281828459045235;
-      uint256 time = PRBMathUD60x18.div(blocksStaked, PRBMathUD60x18.fromUint(1000));
+      uint256 secondsPerYear = 31536000;
+      uint256 percentageYearStaked = PRBMathUD60x18.div(secondsStaked, PRBMathUD60x18.fromUint(secondsPerYear));
       uint256 currStakedNorm = _accounts[msg.sender].stakedAmount;
       uint256 currStaked = PRBMathUD60x18.fromUint(currStakedNorm);
     
-      uint256 rateMulTime = PRBMathUD60x18.mul(interest, time);
-      uint256 totalInterest = PRBMathUD60x18.pow(euler, rateMulTime);
-      uint256 newAmount = PRBMathUD60x18.mul(currStaked, totalInterest);
+      uint256 rateTime = PRBMathUD60x18.mul(interest, percentageYearStaked);
+      uint256 compoundedRate = PRBMathUD60x18.pow(euler, rateTime);
+      uint256 newAmount = PRBMathUD60x18.mul(currStaked, compoundedRate);
       uint256 newAmountNorm = PRBMathUD60x18.toUint(newAmount);
 
       _accounts[msg.sender].stakedAmount = newAmountNorm;
-      _accounts[msg.sender].blockHeight = block.number;
+      _accounts[msg.sender].time = timeStamp;
     }
     
     function BeginPayout() public {
@@ -100,42 +104,40 @@ contract FixedAnnuityStaking is ERC20 {
         _accounts[msg.sender].status = 0;
         
         uint256 currStakedNorm = _accounts[msg.sender].stakedAmount;
-        uint256 currStaked = PRBMathUD60x18.fromUint(currStakedNorm);
-          
-        _accounts[msg.sender].status = 0;
-        uint256 withdrawWindowInBlocksNorm = 10000;
-        uint256 withdrawWindowInBlocks = PRBMathUD60x18.fromUint(withdrawWindowInBlocksNorm);
-        // Set Withdraw Amount Per Block
-        uint256 withdrawAmountPerBlock = PRBMathUD60x18.div(currStaked, withdrawWindowInBlocks);
-        uint256 withdrawAmountPerBlockNorm = PRBMathUD60x18.toUint(withdrawAmountPerBlock);
-      
-        _accounts[msg.sender].withdrawAmountPerBlock = withdrawAmountPerBlockNorm;
+        _accounts[msg.sender].payoutAmountSnapshot = currStakedNorm;
       }
     }
     
     function CollectPayout() public {
         
       BeginPayout();
+      
+      uint256 payoutAmountSnapshotNorm = _accounts[msg.sender].payoutAmountSnapshot;
+      uint256 payoutAmountSnapshot = PRBMathUD60x18.fromUint(payoutAmountSnapshotNorm);
+      uint256 payoutPeriodInSecondsNorm = 31536000;
+      uint256 payoutPeriodInSeconds = PRBMathUD60x18.fromUint(payoutPeriodInSecondsNorm);
         
-      uint256 withdrawAmountPerBlock = PRBMathUD60x18.fromUint(_accounts[msg.sender].withdrawAmountPerBlock);
-      uint256 lastBlockHeight = _accounts[msg.sender].blockHeight;
-      uint256 blocksStakedNorm = block.number - lastBlockHeight;
-      uint256 blocksStaked = PRBMathUD60x18.fromUint(blocksStakedNorm);
-      uint256 blockNumber = block.number;
-      uint256 maxPayoutAmount = PRBMathUD60x18.mul(withdrawAmountPerBlock, blocksStaked);
+      //uint256 timeStamp = block.timeStamp;
+      uint256 timeStamp = fakeBlockTimestamp;
+      uint256 lastTime = _accounts[msg.sender].time;
+      uint256 timePassedInSecondsNorm = timeStamp - lastTime;
+      uint256 timePassedInSeconds = PRBMathUD60x18.fromUint(timePassedInSecondsNorm);
+      uint256 payoutPeriodPercentage = PRBMathUD60x18.div(timePassedInSeconds, payoutPeriodInSeconds);
+      
+      uint256 maxPayoutAmount = PRBMathUD60x18.mul(payoutPeriodPercentage, payoutAmountSnapshot);
       uint256 maxPayoutAmountNorm = PRBMathUD60x18.toUint(maxPayoutAmount);
       uint256 currStakedNorm = _accounts[msg.sender].stakedAmount;
       uint256 currStaked = PRBMathUD60x18.fromUint(currStakedNorm);
     
       uint256 payoutAmount;
       if (currStakedNorm < maxPayoutAmountNorm) {
-          payoutAmount = currStakedNorm;
+        payoutAmount = currStakedNorm;
       } else {
         payoutAmount = maxPayoutAmountNorm;
       }
       
       _sendAmount(payoutAmount);
-      _accounts[msg.sender].blockHeight = block.number;
+      _accounts[msg.sender].time = timeStamp;
       _accounts[msg.sender].stakedAmount -= payoutAmount;
     }
     
@@ -144,5 +146,4 @@ contract FixedAnnuityStaking is ERC20 {
         increaseAllowance(msg.sender, amount);
     }
     
-
 }
