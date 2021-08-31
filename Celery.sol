99e601d99e7080ae94660a05b7e51578e8fb5e92
MaxHastings <max.jacob.hastings@gmail.com>
2021-08-30 21:52:47 -0500

    Update Celery.sol

--- Celery.sol
+++ Celery.sol
@@ -24,7 +24,7 @@ contract Celery is ERC20 {
         return _accounts[addr].stakedAmount;
     }
     
-    function getStakedTime(address addr) public view returns (uint256) {
+    function getLastProcessedTime(address addr) public view returns (uint256) {
         return _accounts[addr].time;
     }
     
@@ -32,44 +32,141 @@ contract Celery is ERC20 {
         return _accounts[addr].payoutAmountSnapshot;
     }
     
-    function getStakedStatus(address addr) public view returns (int) {
+    function getStatus(address addr) public view returns (int) {
         return _accounts[addr].status;
     }
     
-    function IncreaseStake(uint256 amount) public {
-      _transfer(msg.sender, address(this), amount);
+    
+    /*
+    Public function
+    Start the Staking Phase for Account.
+    */
+    function StartStake() public {
       int status = _accounts[msg.sender].status;
+      
+      // Check if Account is in Payout Phase.
       if (status == 0) {
-        CollectPayout();
+          
+        // Process Payout
+        _processPayout();
+        
+        // Set Account to staking phase
+        _accounts[msg.sender].status = 1;
+      }
+    }
+    
+    /*
+    Public Function
+    Add more tokens to your staking account.
+    */
+    function IncreaseStake(uint256 amount) public {
+    
+     // Check if amount is greater than zero.
+      require(amount > 0, "Value must be greater than zero.");
+      
+      // Transfer tokens into smart contract balance.
+      _transfer(msg.sender, address(this), amount);
+      
+      int status = _accounts[msg.sender].status;
+      // Check if Account is in payout phase.
+      if (status == 0) {
+          
+        // Process Payout.
+        _processPayout();
+        
+        // Change Account to Staking Phase.
         _accounts[msg.sender].status = 1;
       } else {
-         calcStake();
+          
+         // If already staking, process current staked balance before adding additional tokens to staked balance.
+         _processStake();
       }
+      
+      // Add additional tokens to staked balance.
       _accounts[msg.sender].stakedAmount += amount;
     }
     
-    function BeginStake() public {
+    /*
+    Public function
+    Start the Payout Phase for Account.
+    */
+    function StartPayout() public {
       int status = _accounts[msg.sender].status;
-      if (status == 0) {
-        CollectPayout();
-        _accounts[msg.sender].status = 1;
+      
+      // Check if Account is staking.
+      if (status == 1) {
+        // Process Stake.
+        _processStake();
+        // Set Account to payout phase.
+        _accounts[msg.sender].status = 0;
+        
+        // Take a snapshot of staked amount. Used later for calculating payout amount.
+        uint256 currStakedNorm = _accounts[msg.sender].stakedAmount;
+        _accounts[msg.sender].payoutAmountSnapshot = currStakedNorm;
       }
     }
-
-    function calcStake() private {
+    
+    function CollectPayout() public {
+    
+      // Start Payout Phase if account is Staking.
+      StartPayout();
+      
+      // Process Payout.
+      _processPayout();
+      
+    }
+    
+    /*
+    Private Function
+    Calculates and adds the interest earned to the staked account balance.
+    Formula used for calculating interest. Continously compounding interest.
+    
+    P * e^(r * t) = P(t)
+    
+    P = Princpal Sum
+    P(t) = Value at time t
+    t = length of time the interest is applied for
+    r = Annual Interest Rate = APR
+    e = Euler's Number = 2.718...
+    APR = 0.69314...%
+    APY = 100%
+    
+    Steps:
+    
+    1) Find out the length of time that has passed since the last time calculating interest for account.
+    2) Use continously compounding formula to calculate the new Princpal sum.
+    3) Save new Princpal sum to account and update the time since last calculation.
+    
+    */
+    function _processStake() private {
+      
       uint256 lastTime = _accounts[msg.sender].time;
       uint256 timeStamp = block.timestamp;
       uint256 secondsStakedNorm = timeStamp - lastTime;
+      
+      // If Time passed is zero end payout process early.
+      if(secondsStakedNorm == 0) {
+          return;
+      }
+      
+      uint256 currStakedNorm = _accounts[msg.sender].stakedAmount;
+      // If Current Staked Amount is zero end payout process early.
+      if (currStakedNorm == 0) {
+          // Update last time account was processed.
+          _accounts[msg.sender].time = timeStamp;
+          return;
+      }
+      
       uint256 secondsStaked = PRBMathUD60x18.fromUint(secondsStakedNorm);
       
-      // Annual interest of 50% continuous compounding.
+      // APY 100% interest
+      // APR 69.314..% continously compounded interest rate
       // Interest represented as 60.18-decimal fixed-point number
-      uint256 interest = 500000000000000000;
+      uint256 interest = 693147180559945309;
       // Euler's number represented as 60.18-decimal fixed-point number
       uint256 euler = 2718281828459045235;
       uint256 secondsPerYear = 31536000;
       uint256 percentageYearStaked = PRBMathUD60x18.div(secondsStaked, PRBMathUD60x18.fromUint(secondsPerYear));
-      uint256 currStakedNorm = _accounts[msg.sender].stakedAmount;
       uint256 currStaked = PRBMathUD60x18.fromUint(currStakedNorm);
     
       uint256 rateTime = PRBMathUD60x18.mul(interest, percentageYearStaked);
@@ -81,57 +178,105 @@ contract Celery is ERC20 {
       _accounts[msg.sender].time = timeStamp;
     }
     
-    function BeginPayout() public {
-      int status = _accounts[msg.sender].status;
-      if (status == 1) {
-        calcStake();
-        _accounts[msg.sender].status = 0;
-        
-        uint256 currStakedNorm = _accounts[msg.sender].stakedAmount;
-        _accounts[msg.sender].payoutAmountSnapshot = currStakedNorm;
-      }
-    }
     
-    function CollectPayout() public {
+    /*
+    Private Function
+    Calculates and processes the payout back to the account address.
+    
+    Steps:
+    
+    1) Find out the length of time that has passed since the last time calculating payout for account.
+    2) Calculate the max payout amount.
+        a) Payout Percentage = Seconds Passed / Year In Seconds
+        b) Max Payout Amount = Payout Percentage * Staked Amount snapshot
+    3) Send either Max Payout Amount or Current Staked Amount whichever is smaller to the Account address.
+    4) Update Account last calculation time and subtract payout amount from account staked amount.
         
-      BeginPayout();
+    */
+    function _processPayout() private {
       
-      uint256 payoutAmountSnapshotNorm = _accounts[msg.sender].payoutAmountSnapshot;
-      uint256 payoutAmountSnapshot = PRBMathUD60x18.fromUint(payoutAmountSnapshotNorm);
-      // 1 year payout period in Seconds
-      uint256 payoutPeriodInSecondsNorm = 31536000;
-      uint256 payoutPeriodInSeconds = PRBMathUD60x18.fromUint(payoutPeriodInSecondsNorm);
-        
+      // Get the latest block timestamp.
       uint256 timeStamp = block.timestamp;
+      
+      // Get the last time account payout was processed.
       uint256 lastTime = _accounts[msg.sender].time;
+      
+      // Calculate the length of time that has passed.
       uint256 timePassedInSecondsNorm = timeStamp - lastTime;
+      
+      // If Time passed is zero end payout process early.
+      if(timePassedInSecondsNorm == 0) {
+          return;
+      }
+      
+      // Get Current Staked Amount
+      uint256 currStakedNorm = _accounts[msg.sender].stakedAmount;
+      
+      // If Current Staked Amount is zero end payout process early.
+      if (currStakedNorm == 0) {
+          // Update last time account was processed.
+          _accounts[msg.sender].time = timeStamp;
+          return;
+      }
+        
+      // Get Account Payout Snapshot Amount.
+      uint256 payoutAmountSnapshotNorm = _accounts[msg.sender].payoutAmountSnapshot;
+      // Convert Account Payout Snapshot Amount into Fixed Point Decimal.
+      uint256 payoutAmountSnapshot = PRBMathUD60x18.fromUint(payoutAmountSnapshotNorm);
+      // 1 year payout period in seconds. 60 * 60 * 24 * 365 = 31536000 seconds in a year.
+      uint256 payoutPeriodInSecondsNorm = 31536000;
+      // Convert Payout Period into Fixed Point Decimal.
+      uint256 payoutPeriodInSeconds = PRBMathUD60x18.fromUint(payoutPeriodInSecondsNorm);
+      
+      // Convert length of time to Fixed Point Decimal.
       uint256 timePassedInSeconds = PRBMathUD60x18.fromUint(timePassedInSecondsNorm);
+      
+      // Calculate percentage of the year that has passed.
       uint256 payoutPeriodPercentage = PRBMathUD60x18.div(timePassedInSeconds, payoutPeriodInSeconds);
       
+      // Multiply percentage of year with the payout staked snapshot amount.
       uint256 maxPayoutAmount = PRBMathUD60x18.mul(payoutPeriodPercentage, payoutAmountSnapshot);
+      
+      // Convert payout amount back to normal integer.
       uint256 maxPayoutAmountNorm = PRBMathUD60x18.toUint(maxPayoutAmount);
-      uint256 currStakedNorm = _accounts[msg.sender].stakedAmount;
     
       uint256 payoutAmount;
+      
+      // Check if Current Staked Amount is smaller than Max Payout
       if (currStakedNorm < maxPayoutAmountNorm) {
+        
+        // Choose Current Staked Amount if it is smaller.
         payoutAmount = currStakedNorm;
       } else {
+          
+        // Choose Max Payout Amount if it is smaller.
         payoutAmount = maxPayoutAmountNorm;
       }
       
+      // Send tokens to account address.
       _sendAmount(payoutAmount);
+      // Update last time account was processed.
       _accounts[msg.sender].time = timeStamp;
+      // Subtract payout amount from staked amount.
       _accounts[msg.sender].stakedAmount -= payoutAmount;
     }
     
+    /*
+    Private Fucntion
+    Sends Tokens to account address.
+    */
     function _sendAmount(uint256 amount) private {
         
+        // Get how many tokens are in contract address.
         uint256 contractTokenHoldings = balanceOf(address(this));
         
+        // Check if contract address has enough tokens to send.
         if(amount > contractTokenHoldings) {
+            // If contract does not have enough tokens mint more.
             _mint(msg.sender, amount);
             increaseAllowance(msg.sender, amount);
         } else {
+            // If contract has enough tokens transfer them.
             _transfer(address(this), msg.sender, amount);
         }
     }
