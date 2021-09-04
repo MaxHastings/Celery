c667329b6c89da9d80c9a238fcbcca1c365d26c1
Miles <30772334+linkinpark9812@users.noreply.github.com>
2021-09-04 02:16:17 -0500

    Update TokenSale.sol
    Potential improvements

--- TokenSale.sol
+++ TokenSale.sol
@@ -1,45 +1,54 @@
 //SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.2;
+pragma solidity 0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 contract TokenSale {
     
-    IERC20Metadata public tokenContract;  // the token being sold
-    uint256 public price;              // the price, in wei, per token
-    address owner;
+    IERC20Metadata public TokenContract;  // the token being sold
+    uint256 public TokenPrice;              // the price, in wei, per token
+    uint256 public TokensSold = 0;
+    bool public SaleActive = true;
+    address private _owner;
 
-    constructor(IERC20Metadata _tokenContract, uint256 _price) {
-        owner = msg.sender;
-        tokenContract = _tokenContract;
-        price = _price;
+    constructor(IERC20Metadata tokenContract, uint256 price) {
+        _owner = msg.sender;
+        TokenContract = tokenContract;
+        TokenPrice = price;
     }
-    
-    uint256 public tokensSold;
 
     event Sold(address buyer, uint256 amount);
     
-    function buyTokens(uint256 numberOfTokens) public payable {
-        require(msg.value == (numberOfTokens * price));
+    function BuyTokens(uint256 numberOfTokens) public payable {
+        require(SaleActive, "The Celery token sale has ended");
+        require(msg.value == (numberOfTokens * TokenPrice), "Your BCH and Celery ratio amounts must match per the price");
     
-        uint256 scaledAmount = numberOfTokens * (uint256(10) ** tokenContract.decimals());
+        uint256 scaledAmount = numberOfTokens * (uint256(10) ** TokenContract.decimals());
     
-        require(tokenContract.balanceOf(address(this)) >= scaledAmount);
+        require(TokenContract.balanceOf(address(this)) >= scaledAmount, "Token sale contract does not have enough Celery");
     
         emit Sold(msg.sender, numberOfTokens);
-        tokensSold += numberOfTokens;
+        TokensSold += numberOfTokens;
     
-        require(tokenContract.transfer(msg.sender, scaledAmount));
+        require(TokenContract.transfer(msg.sender, scaledAmount));
+    }
+
+    function StartSale() public {
+        _ownerCheck();
+        SaleActive = true;
     }
     
-    function endSale() public {
-        require(msg.sender == owner);
-    
+    function EndSale() public {
+        _ownerCheck();
         // Send unsold tokens to the owner.
-        require(tokenContract.transfer(owner, tokenContract.balanceOf(address(this))));
+        require(TokenContract.transfer(_owner, TokenContract.balanceOf(address(this))));
     
         payable(msg.sender).transfer(address(this).balance);
+
+        SaleActive = false;
     }
 
-
+    function _ownerCheck() private view {
+        require(msg.sender == _owner, "You must be the owner");
+    }
 }
