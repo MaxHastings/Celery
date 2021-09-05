861280d8c355ec335863cb84d4e32f5cd2e664be
MaxHastings <max.jacob.hastings@gmail.com>
2021-09-04 21:32:14 -0500

    added test for staking doubling

--- test/Celery.test.js
+++ test/Celery.test.js
@@ -1,16 +1,17 @@
 // test/Celery.test.js
 // Load dependencies
 const { expect } = require("chai");
+const hre = require("hardhat");
 
 // Start test Celery
 describe("Celery", function () {
   before(async function () {
-    this.Celery = await ethers.getContractFactory("Celery");
+    this.CeleryFactory = await ethers.getContractFactory("Celery");
     this.owner = (await ethers.getSigners())[0];
   });
 
   beforeEach(async function () {
-    this.Celery = await this.Celery.deploy(10000000);
+    this.Celery = await this.CeleryFactory.deploy(100000000);
     await this.Celery.deployed();
   });
 
@@ -23,5 +24,22 @@ describe("Celery", function () {
     expect(
       (await this.Celery.getStatus(this.owner.address)).toString()
     ).to.equal("1");
+  });
+
+  // Test case
+  it("Test if staking amount doubles in a year", async function () {
+    // Increase Stake
+    await this.Celery.IncreaseStake(100000000);
+
+    // Wait 1 year in block time
+    await hre.network.provider.send("evm_increaseTime", [31536000]);
+
+    // Start payout
+    await this.Celery.StartPayout();
+
+    // Test if account staking balance doubled
+    expect(
+      (await this.Celery.getStakedAmount(this.owner.address)).toString()
+    ).to.equal("200000000");
   });
 });
