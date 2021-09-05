eea1e7b56fd95b41dee55723911cd4d1a3e92b17
MaxHastings <max.jacob.hastings@gmail.com>
2021-09-05 13:36:32 -0500

    refactored and fixed block time increase

--- test/Celery.test.js
+++ test/Celery.test.js
@@ -40,89 +40,6 @@ describe("Celery", function () {
     await expectAccountBalance(this.owner.address, 0);
   });
 
-  // Helper function for account staking of time length
-  async function StakeAmountForTime(amount, time) {
-    // Increase Stake
-    await Celery.IncreaseStake(amount);
-
-    const timeStaked = await Celery.getLastProcessedTime(this.owner.address);
-
-    // Test if account status is staking
-    await expectStatus(this.owner.address, 1);
-
-    // Wait seconds in block time
-    await increaseBlockTime(time);
-
-    // Start payout
-    await Celery.StartPayout();
-
-    // Test if last processed time is correct
-    expectLastProcessedTime(
-      this.owner.address,
-      BigNumber.from(time).add(timeStaked)
-    );
-
-    // Test if account status is payout
-    await expectStatus(this.owner.address, 0);
-
-    // Test current payout amount is correct
-    const calcStakeAmount = calculateStake(amount, 0, time);
-    await expectPayoutAmount(this.owner.address, calcStakeAmount);
-
-    // Test staked amount is correct
-    await expectStakedAmount(this.owner.address, calcStakeAmount);
-  }
-
-  // *** Helper Functions *** //
-
-  function calculateStake(amount, startTime, endTime) {
-    const secondsInAYear = 31536000;
-    const diffTime = endTime - startTime;
-    const percTime = diffTime / secondsInAYear;
-    return Math.round(amount * Math.pow(Math.E, percTime * Math.LN2));
-  }
-
-  // *** Expect Functions *** //
-
-  async function expectAccountBalance(address, amount) {
-    expect((await Celery.balanceOf(address)).toString()).to.equal(
-      amount.toString()
-    );
-  }
-
-  async function expectStakedAmount(address, amount) {
-    expect((await Celery.getStakedAmount(address)).toString()).to.equal(
-      amount.toString()
-    );
-  }
-
-  async function expectPayoutAmount(address, amount) {
-    expect((await Celery.getCurrentPayoutAmount(address)).toString()).to.equal(
-      amount.toString()
-    );
-  }
-
-  async function expectLastProcessedTime(address, time) {
-    expect((await Celery.getLastProcessedTime(address)).toString()).to.equal(
-      time.toString()
-    );
-  }
-
-  async function expectStatus(address, status) {
-    expect((await Celery.getStatus(address)).toString()).to.equal(
-      status.toString()
-    );
-  }
-
-  async function expectTotalSupply(amount) {
-    expect((await Celery.totalSupply()).toString()).to.equal(amount.toString());
-  }
-
-  async function increaseBlockTime(time) {
-    // Wait half year in block time
-    await hre.network.provider.send("evm_increaseTime", [time]);
-  }
-
   // Test case
   it("Test if payout is half amount in a year", async function () {
     await StakeAmountForTime.bind(this)(100000000, 31536000);
@@ -229,3 +146,88 @@ describe("Celery", function () {
     await expectAccountBalance(this.owner.address, 100000000);
   });
 });
+
+// *** Helper Functions *** //
+
+// Helper function for account staking of time length
+async function StakeAmountForTime(amount, time) {
+  // Increase Stake
+  await Celery.IncreaseStake(amount);
+
+  const timeStaked = await Celery.getLastProcessedTime(this.owner.address);
+
+  // Test if account status is staking
+  await expectStatus(this.owner.address, 1);
+
+  // Wait seconds in block time
+  await increaseBlockTime(time);
+
+  // Start payout
+  await Celery.StartPayout();
+
+  // Test if last processed time is correct
+  expectLastProcessedTime(
+    this.owner.address,
+    BigNumber.from(time).add(timeStaked)
+  );
+
+  // Test if account status is payout
+  await expectStatus(this.owner.address, 0);
+
+  // Test current payout amount is correct
+  const calcStakeAmount = calculateStake(amount, 0, time);
+  await expectPayoutAmount(this.owner.address, calcStakeAmount);
+
+  // Test staked amount is correct
+  await expectStakedAmount(this.owner.address, calcStakeAmount);
+}
+
+function calculateStake(amount, startTime, endTime) {
+  const secondsInAYear = 31536000;
+  const diffTime = endTime - startTime;
+  const percTime = diffTime / secondsInAYear;
+  return Math.round(amount * Math.pow(Math.E, percTime * Math.LN2));
+}
+
+// *** Expect Functions *** //
+
+async function expectAccountBalance(address, amount) {
+  expect((await Celery.balanceOf(address)).toString()).to.equal(
+    amount.toString()
+  );
+}
+
+async function expectStakedAmount(address, amount) {
+  expect((await Celery.getStakedAmount(address)).toString()).to.equal(
+    amount.toString()
+  );
+}
+
+async function expectPayoutAmount(address, amount) {
+  expect((await Celery.getCurrentPayoutAmount(address)).toString()).to.equal(
+    amount.toString()
+  );
+}
+
+async function expectLastProcessedTime(address, time) {
+  expect((await Celery.getLastProcessedTime(address)).toString()).to.equal(
+    time.toString()
+  );
+}
+
+async function expectStatus(address, status) {
+  expect((await Celery.getStatus(address)).toString()).to.equal(
+    status.toString()
+  );
+}
+
+async function expectTotalSupply(amount) {
+  expect((await Celery.totalSupply()).toString()).to.equal(amount.toString());
+}
+
+// Increase block timestamp by number of seconds
+async function increaseBlockTime(time) {
+  const lastBlockTime = (await ethers.provider.getBlock("latest")).timestamp;
+  const nextBlockTime = lastBlockTime + time - 1;
+  await hre.network.provider.send("evm_mine", [nextBlockTime]);
+}
