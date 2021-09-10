2dfb6035272654e2b87b5d7a06893049a9e5feb8
Miles <30772334+linkinpark9812@users.noreply.github.com>
2021-09-10 13:57:26 -0500

    deploy setup

---
+++ scripts/deploy.js
@@ -0,0 +1,17 @@
+async function main() {
+    // We get the contract to deploy
+    const Celery = await ethers.getContractFactory("Celery");
+    const celery = await Celery.deploy("500000000000000000000");
+
+    console.log("Celery deployed!");
+    console.log(`Contract Address: ${celery.address}`);
+    console.log(`Contract From: ${celery.deployTransaction.from}`);
+    console.log(`Contract TX Id: ${celery.deployTransaction.hash}`);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
